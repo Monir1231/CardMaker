2f00d036fa1b6ca9032fd4106768de940e228949
Monir1231 <monirmolla324@gmail.com>
2025-08-04 17:33:00 +0600

    commn done

--- src/data/data.js
+++ src/data/data.js
@@ -49,3 +49,96 @@
         rainText:"Blizzard"
     },
  ]
+
+ export const IncidentLink =[
+    {
+        img:"IncidentImg-1.svg",
+        title:"What type of incident?",
+        des:"Choose the category that best describes the incident."
+    },
+     {
+        img:"IncidentImg-2.svg",
+        title:"Tell us about the incident?",
+        des:"Let’s connect the dots and see where to start."
+    },
+     {
+        img:"IncidentImg-3.svg",
+        title:"Where did the incident occur?",
+        des:"Lorem ipsum dolar sit general sac mascho werho"
+    }
+ ]
+
+ export const Icondata=[
+    {
+        img:"Icon-1.png",
+        text:"Avalanche"
+    },
+    {
+        img:"Icon-2.png",
+        text:"Biological"
+    },
+    {
+        img:"Icon-3.png",
+        text:"Blizzard"
+    },
+    {
+        img:"Icon-4.png",
+        text:"Cold/Freezing"
+    },
+    {
+        img:"Icon-5.png",
+        text:"Drought"
+    },
+    {
+        img:"Icon-6.png",
+        text:"Earthquake"
+    },
+    {
+        img:"Icon-7.png",
+        text:"Flooding"
+    },
+    {
+        img:"Icon-8.png",
+        text:"Heat Wave"
+    },
+    {
+        img:"Icon-9.png",
+        text:"Hail"
+    },
+    {
+        img:"Icon-10.png",
+        text:"Lightning"
+    },
+    {
+        img:"Icon-11.png",
+        text:"Man Made"
+    },
+    {
+        img:"Icon-12.png",
+        text:"Mudslide"
+    },
+    {
+        img:"Icon-13.png",
+        text:"Severe Storm"
+    },
+    {
+        img:"Icon-14.png",
+        text:"Strong Wind"
+    },
+    {
+        img:"Icon-15.png",
+        text:"Tornado"
+    },
+    {
+        img:"Icon-16.png",
+        text:"Tsunami"
+    },
+    {
+        img:"Icon-17.png",
+        text:"Volcanic Eruption"
+    },
+    {
+        img:"Icon-18.png",
+        text:"Wildfire"
+    },
+ ]
